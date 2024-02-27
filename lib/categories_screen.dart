a31576a54e8f13a444588baca712e4206b4fe007
Queengirl1990 <bruehlanja90@gmail.com>
2024-02-27 09:38:53 +0100

    themes

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/category_item.dart';
 import 'dummy-data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map(
-            (catData) => CategoryItem(catData.title, catData.color),
-          )
-          .toList(),
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          // Sliver kümmert sich ums scrollen GridDelegate um die größe der Grids
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2, //3/2 Verhältnis breite x höhe
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20), //Abstand zwischen den Elemeten
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(catData.title, catData.color),
+            )
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            // Sliver kümmert sich ums scrollen GridDelegate um die größe der Grids
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2, //3/2 Verhältnis breite x höhe
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20), //Abstand zwischen den Elemeten
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/categories_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,30 +11,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeals'),
-      ),
-      body: const Center(
-        child: Text('Navigation Time!'),
-      ),
+          primarySwatch: Colors.pink,
+          hintColor: Colors.amber,
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1))),
+              body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              title: prefix0.TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed')
+              )
+              ),
+      home: const CategoriesScreen(),
     );
   }
 }
